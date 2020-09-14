654660fe8a42f78aad7af53caf9d6c2cdb4c2016
orangebit-tech <38854207+orangebit-tech@users.noreply.github.com>
2020-09-14 02:25:47 -0700

    router setup

--- src/router/router.js
+++ src/router/router.js
@@ -1,24 +1,63 @@
 import Vue from 'vue/dist/vue.min.js';
-import VueRouter from 'vue-router'
-import App from '../App.vue'
+
+import VueRouter        from 'vue-router'
+import App              from '../App.vue'
+import Home             from '../components/Home.vue'
+import Overview         from '../components/Overview.vue'
+import JobExperience    from '../components/JobExperience.vue'
+import Skills           from '../components/Skills.vue'
+import Projects         from '../components/Projects.vue'
+import Contacts         from '../components/Contacts.vue'
 
 
 
-Vue.use(VueRouter)
-// Vue.use(VueResource)
+
 
 export const routes = [
     {
         path: "/",
-        redirect: "/home",
         name: "App",
-        components: {
-            default: App
-        },
-        children: []
+        component: App,
+        redirect: 'home',
+        children: [
+            {
+                path: "home",
+                name: "Home",
+                component: Home,
+                children: [
+                    {
+                        path: "/home/overview",
+                        name: "Overview",
+                        component: Overview,
+                    },
+                    {
+                        path: "/home/jobexperience",
+                        name: "Job Experience",
+                        component: JobExperience,
+                    },
+                    {
+                        path: "/home/skills",
+                        name: "Skills",
+                        component: Skills,
+                    },
+                    {
+                        path: "/home/projects",
+                        name: "Projects",
+                        component: Projects,
+                    },
+                    {
+                        path: "/home/contacts",
+                        name: "Contacts",
+                        component: Contacts,
+                    },
+                ]
+            },
+            
+        ]
     },
+    
 ]
-
+Vue.use(VueRouter)
 
 export const router = new VueRouter({
     mode: 'history',
