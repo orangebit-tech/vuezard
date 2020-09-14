d6f0dabf122d082734a9171f60618d9168c5da1f
orangebit-tech <38854207+orangebit-tech@users.noreply.github.com>
2020-09-14 02:23:50 -0700

    router integrated

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,15 @@
     <div class="header">
       <Header/>
     </div>
-    <div class="main-frame">
-   <p class="blue" href="">hello</p>
+    <div class="router">
+      <router-view></router-view>
     </div>
- 
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-// import scrapedin from 'scrapedin'
+// import Vue from 'vue'
 
 export default {
   name: 'App',
@@ -21,86 +20,188 @@ export default {
   },
   data(){
     return {
-      
+
+      myGitHubData: {}
     }
   },
   methods: {
-    async scrap(){
-      const scrapedin = require('scrapedin')
-        const profileScraper = await scrapedin({ email: 'login@mail.com', password: 'pass' })
-        const profile = await profileScraper('https://www.linkedin.com/in/some-profile/')
-      return profile
-
-
+    async get(){
+      return await this.GitHubAPI.get('/user', {}, [this.myGitHubData, 'user'])
     }
   },
   created () {
-    console.log(this.scrap())
   }
 }
 </script>
 
 <style>
 @import '/assets/style.css'; 
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #29658A;
-
-
-}
-html {
-  
+  color: #45A29F;
 }
 body {
-  background-color: #25274D !important;
-  
+  padding-left: 10%;
+  padding-right: 10%;
+  background-color: #0B0C10 !important;
 }
-.app {
-
+h1 {
+  color: #C5C6C8;
+}
+h3 {
+ color: #66FCF1;
+ font-weight: 200;
 } 
+.main-frame {
+
+  border: 1px solid #202833;
+  height: 175px;
+  width: 100%;
+}
+.info {
+  display: inline-block;
+  padding-top: 20px;
+}
+.image {
+  display: inline-block;
+  border: 3px solid #45A29F;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+
+}
+.png {
+  max-width: 105px;
+}
+.initials {
+  color: #45A29F;
+  margin-top: 20px;
+  margin-left: 2px;
+  letter-spacing: 4px;
+}
+.smile {
+  border-bottom: 6px solid #45A29F;
+  border-radius: 100%;
+  width: 70px;
+  height: 30px;
+  margin-left: 25px;
+  margin-top: -18px;
+}
+.title {
+  margin-left: 25px;
+  vertical-align: top;
+  display: inline-block;
+}
 .header {
   margin-bottom: 130px;
 }
-.main-frame {
-  border: 1px solid #29658A;
-  height: 300px;
+.buttons {
+  margin-top: 27px;
+}
+.button {
+  font-weight: 200;
+  font-size: 18px;
+  border: 1px solid #202833;
+  padding: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-left: 20px;
+  border-radius: 5px;
+  transition: 0.3s;
+  text-decoration: none;
+  color: #C5C6C8
+}
+.button:hover {
+  color: #66FCF1;
+  border: 1px solid  #66FCF1;
+  cursor: pointer;
+}
+.is-active {
+  width: static;
+  /* font-weight: 600; */
+  background-color: #66FCF1;
+  color: #0B0C10 !important;
+  border: 1px solid  #66FCF1;
+}
+.router-2 {
+  margin-top: 25px;
+  border: 1px solid  #202833;
+  height: 400px;
+  padding: 15px;
+}
+.block {
+  color: #C5C6C8;
+  border: 1px solid #202833;
+  width: 250px;
+  height: 250px;
+  margin: 20px;
+  vertical-align: top;
+}
+.block-inline {
+  display: inline-block;
+
+}
+.block-left {
+  width: 530px ;
+  /* padding-left: 0px; */
+}
+.block-right {
+  width: 530px;
+    padding-left: 20px !important;
+  padding-right: 20px !important;
+  
+}
+.summary {
+  padding-left: 20px !important;
+  padding-right: 20px !important;
+  /* letter-spacing: 0.06rem; */
+  line-height: 1.2rem;
+  text-align: left;
+  /* padding-left: 20px; */
+  /* padding-right: 15px; */
 }
 
-/* Palette */
-.black {
-  color: #25274D;
-}
-.grey {
-  color: #474866;
-}
-.lightgrey {
-  color: #A9ABB8;
-}
-.blue {
-  color: #2D9CCA;
-}
-.darkblue {
-  color: #29658A;
-}
+.loading{
+  
 
-.bg-black {
-  background-color: #25274D;
 }
-.bg-grey {
-  background-color: #474866;
+.spinner {
+  text-align: center !important;
+  vertical-align: center;
+  margin-left: 46%;
+  margin-top: 12%;
 }
-.bg-lightgrey {
-  background-color: #A9ABB8;
+.gh-contributions {
+  width: 40%;
+  border: 1px solid #202833;
+  vertical-align: top;
+  padding-top: 0px;
+  padding-bottom: 20px;
+  display: inline-block;
+  
 }
-.bg-blue {
-  background-color: #2D9CCA;
-}
-.bg-darkblue {
-  background-color: #29658A;
-}
+.contribs {
+  text-align: center;
 
+}
+.widget-title {
+  color: #45A29F;
+  font-size: 16px;
+}
+.widget-number {
+  color: #66FCF1;
+}
+.chart {
+  width: 100%;
+  display: inline-block;
+  border: 1px solid #202833;
+}
+.experience{
+   width: auto;
+}
 
 </style>
