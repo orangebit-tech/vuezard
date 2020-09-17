cd6d9ad9da0c075e88949dee47f6f95507cdd90b
orangebit-tech <38854207+orangebit-tech@users.noreply.github.com>
2020-09-17 16:21:49 -0700

    polished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="main">
-   
+
     </div>
 </template>
 
 <script>
 export default {
-    
+    name
 }
 </script>
 
 <style scoped>
 .main{
-   
+   background-color: #45A29F;
 }
 </style>
