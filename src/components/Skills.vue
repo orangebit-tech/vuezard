285680b4493de2830e3411a096547f9f95bf282f
orangebit-tech <38854207+orangebit-tech@users.noreply.github.com>
2020-09-17 16:23:11 -0700

    skills finished

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,12 +1,69 @@
 <template>
     <div>
-        Overview
+        <div style="text-align: left" class="block block-inline block-left skills-block">
+            <div class="division">
+                <h3 class="label">Technologies</h3>
+                <ul class="skills">
+                    <li class="skill" v-for="(technology, index) in technologies" :key="index">{{technology.name}}</li>
+                </ul>
+            </div>
+            <div class="division">
+                <h3 class="label">Key Skills</h3>
+                <ul class="skills">
+                    <li class="skill" v-for="(skill, index) in skills" :key="index">{{skill.name}}</li>
+                </ul>
+            </div>
+            
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Skills',
-
+    data (){
+        return {
+            technologies: [
+                {
+                    name: 'Vue JS, Vuex, Axios',
+                },
+                {
+                    name: 'Node JS, Adonis JS'
+                },
+                {
+                    name: 'Laravel'
+                },
+                {
+                    name: 'AWS S3, R53, EC2'
+                },
+                {
+                    name: 'Custom / Headless Wordpress'
+                },
+                {
+                    name: 'SQL Databases'
+                },
+            ],
+            skills: [
+                {
+                    name: 'HTML, CSS, JavaScript'
+                },
+                {
+                    name: 'jQuery, Ajax'
+                },
+                {
+                    name: 'PHP'
+                },            
+                {
+                    name: 'Linux Server, VPS'
+                },
+                {
+                    name: 'REST API'
+                },
+                {
+                    name: 'SQL'
+                },
+            ],
+        }
+    }
 }
 </script>
