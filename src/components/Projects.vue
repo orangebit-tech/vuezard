e47785ff1438fb9beb7bf4ccad73a82095ffdc1d
orangebit-tech <38854207+orangebit-tech@users.noreply.github.com>
2020-09-17 16:22:57 -0700

    projects added

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        Overview
+        <div class="block block-inline">
+            <h3>Gixcore</h3>
+        </div>
+        <div class="block block-inline">
+            <h3>The Assistant</h3>
+        </div>
+        <div class="block block-inline">
+            <h3>Electrominer</h3>
+        </div>
     </div>
 </template>
 
