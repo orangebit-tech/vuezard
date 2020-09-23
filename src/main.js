d46ba6afdb6cf407e4f42d352026042544100b94
orangebit-tech <38854207+orangebit-tech@users.noreply.github.com>
2020-09-22 18:35:22 -0700

    deleted gh key

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue                  from 'vue/dist/vue.min.js'
 import { router }           from './router/router'
 import store                from './store'
 import VueResource          from 'vue-resource'
-import GitHubAPI            from 'vue-github-api'
 import App                  from './App.vue'
 import TrendChart           from "vue-trend-chart";
 import Chartkick            from 'vue-chartkick'
@@ -21,7 +20,6 @@ library.add(fab,fas,far)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(Chartkick.use(Chart))
 Vue.use(VueResource)
-Vue.use(GitHubAPI, { token: '5b82185450e1527059aecc1d1cc72b4a5edb9daf' })
 Vue.use(VueClipboard)
 Vue.use(TrendChart);
 
