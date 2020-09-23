221799e4aec5c536ec639e1439abab6a217233c4
orangebit-tech <38854207+orangebit-tech@users.noreply.github.com>
2020-09-22 18:35:11 -0700

    projects added, routing

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,13 +1,24 @@
 <template>
     <div>
-        <div class="block block-inline">
-            <h3>Gixcore</h3>
+        <router-link v-for="(project, index) in projects" :key="index" class="project" :to="project.url">
+            <div class="block block-inline project-widget">
+                <h3>{{project.name}}</h3>
+            </div>
+        </router-link>
+        <div v-if="$route.name == 'Gixcore'" class="description">
+            <p>
+                <a>Gixcore</a> is a web application that lets users build a custom PC. The app has a products compatibility check feature, API connection to Amazon and Newegg, custom CMS. Built on <a>Vue JS</a> and <a>Adonis JS</a> frameworks.
+            </p>
         </div>
-        <div class="block block-inline">
-            <h3>The Assistant</h3>
+        <div v-if="$route.name == 'The Assistant'" class="description">
+            <p>
+                <a>The Assistant</a> is a web application connects customers with local businesses and freelancers. Made on <a>Vue JS</a> and headless <a>WordPress</a>
+            </p>
         </div>
-        <div class="block block-inline">
-            <h3>Electrominer</h3>
+        <div v-if="$route.name == 'Electrominer'" class="description">
+            <p>
+                lorem ipsum Electrominer...
+            </p>
         </div>
     </div>
 </template>
@@ -15,6 +26,31 @@
 <script>
 export default {
     name: 'Projects',
+    data(){
+        return {
+            projects: [
+                {
+                    name: "GixCore",
+                    url: '/home/projects/gixcore',
+                },
+                {
+                    name: "The Assistant",
+                    url: '/home/projects/theassistant',
+                },
+                {
+                    name: "Electrominer",
+                    url: '/home/projects/electrominer',
+                },
+                
+            ]
+        }
+    },
+    methods: {
+
+    },
+    created(){
+        console.log(this.$parent)
+    }
 
 }
 </script>
