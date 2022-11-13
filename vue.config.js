1c88276f782134b7693fb24a442c6e090a7ed05e
orangebit-tech <orangebitt@gmail.com>
2022-11-13 23:49:36 +0000

    dev server

---
+++ vue.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  configureWebpack: {
+    devServer: {
+      watchOptions: {
+        ignored: ['node_modules', 'public'],
+      }
+    }
+  }, 
+  devServer: {
+    // headers: { "Access-Control-Allow-Origin": "*" },
+    // host: 'a3i3.dev',
+    host: 'alpha.gixcore.com',
+    // port: 8080,
+    // proxy: {
+    //   "^/jira/": {
+    //     target: {
+    //       protocol: "https:",
+    //       host: "americor.atlassian.net",   
+    //       port: 443   
+    //     },
+    //     changeOrigin: true,
+    //     pathRewrite: { '^/jira/': '/rest/api/' },
+    //     ws: true,
+    //     // "secure": true,
+    //     /*
+    //     // can't understand what i missing but it does not want to proxy app queries like from "/redmine/api/*" => https://msmonitor.maintstar.co/redmine/api/*
+    //     pathRewrite: function (path, req) {
+    //       console.log(path);
+    //       return path.replace('^/redmine/api', 'asdfasdf');
+    //     },
+    //     */
+    //     logLevel: "debug"
+    //   }
+    // }
+  },
+  transpileDependencies: [
+    'vuetify'
+  ]
+};
